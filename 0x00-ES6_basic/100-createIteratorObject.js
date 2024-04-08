dd1c6540f3dbff8a2b72fd392e5d4651208c0217
othmanoach <othmanoach@gmail.com>
2024-04-08 11:30:08 +0000

    style

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,3 @@
-// The Breakdown 😏😉😉
-// I know it's long, I was just trying to understand how things work
-//
-// Here's the short code using generator function
-//
 export default function createIteratorObject(report) {
   return (function* _() {
     for (const department of Object.values(report.allEmployees)) {
