83d23183dbb80d92fae993cfcac0aa1a94a32bb4
othmanoach <othmanoach@gmail.com>
2024-04-08 11:30:57 +0000

    fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // not using var
-    const task2 = false; // not using var
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
