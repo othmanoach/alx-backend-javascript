5c0fdf0d7c087c007c232f2f7f0adc25ee2ce8bf
othmanoach <othmanoach@gmail.com>
2024-04-18 00:06:29 +0100

    typescript

---
+++ 0x04-TypeScript/README.md
@@ -0,0 +1,8 @@
+# 0x04. Typescript
+| `JavaScript` | `TypeScript` |
+
+## Resources
+**Read or watch:**
+
+- [TypeScript in 5 minutes](https://intranet.alxswe.com/rltoken/waTSa9Mguj912pel9On57w)
+- [TypeScript documentation](https://intranet.alxswe.com/rltoken/iPO8DlHCGzc1jnojLoP9HA)
---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,65 @@
+
+/**
+ * Write an interface named Studentthat accepts the elements:
+ * - firstName(string),
+ * - lastName(string),
+ * - age(number),
+ * - location(string)
+ */
+
+export interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string,
+}
+
+/**
+ * Create two students, and create an array named studentsList
+ */
+
+const student1: Student = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 25,
+  location: 'Nairobi',
+};
+
+const student2: Student = {
+  firstName: 'Alex',
+  lastName: 'Sanchez',
+  age: 29,
+  location: 'Cairo',
+};
+
+const studentsList: Array<Student> = [
+  student1,
+  student2,
+];
+
+
+/**
+ * render a table and for each elements in the array,
+ */
+
+export const renderTable = (studentsList: Array<Student>): void =>  {
+  // create table tag
+  const table = document.createElement('table');
+  const headRow = document.createElement('tr');
+  table.insertAdjacentElement('beforeend', headRow);
+
+  // insert headers
+  headRow.insertAdjacentHTML('beforeend', '<th>FirstName</th>');
+  headRow.insertAdjacentHTML('beforeend', '<th>Location</th>');
+
+  for (const student of studentsList) {
+    const studentRow = document.createElement('tr')
+    studentRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
+    studentRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
+    table.insertAdjacentElement('beforeend', studentRow);
+  }
+
+  document.body.insertAdjacentElement('beforeend', table);
+}
+
+renderTable(studentsList);
